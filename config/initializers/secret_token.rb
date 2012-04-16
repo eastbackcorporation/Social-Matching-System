67dc0cdcd4f08d2af8327da093ab7194264bc693
Junichi Ishikura <ishikura.junichi@mnmleng.com>
2012-04-16 14:03:22 +0900

    Rails initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialMatchingSystem::Application.config.secret_token = '123a9e77637d0f6ce02f450afc76439c8a1cac93ec97b992a35d468c90d293020d5757c75cd49d236132f08af02cdf57c0181af0965286c340ece61a2cee2674'
