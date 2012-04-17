aea4af76ceee1dd97fb0e97d0d8c9f827b5d7c3f
kou honda <kou_honda@eastback.jp>
2012-04-17 09:05:19 +0900

    update database's migration files

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,15 @@
 #ロールの初期設定
 role=%W(admin sender receiver)
 
+Role.delete_all
 role.each do |e|
   Role.create(:name => e)
 end
 
+#admin作成
+admin = User.create(:login => "admin",
+                          :email =>"sms@eastback.jp",
+                          :password => "admin",
+                          :password_confirmation => "admin")
 
+admin.roles<<Role.admin
