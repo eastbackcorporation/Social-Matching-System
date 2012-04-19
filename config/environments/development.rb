169ffd0e4a1b0ef8b33077db95aadde04c060847
kou honda <kou_honda@eastback.jp>
2012-04-19 23:14:16 +0900

    Bug fix at config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,6 @@ SocialMatchingSystem::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  ActionMailer::Base.default_url_options[:host] = "localhost:3000"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
