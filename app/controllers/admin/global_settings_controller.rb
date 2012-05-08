df8c3057beae4236f7ca80773e5a7742eadc5d31
kou honda <kou_honda@eastback.jp>
2012-05-08 14:58:19 +0900

    Add Admin::GlobalSettings Controller and View

---
+++ app/controllers/admin/global_settings_controller.rb
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# 共通設定画面コントローラ
+class Admin::GlobalSettingsController < ApplicationController
+  #編集画面表示
+  def edit
+    @global_setting = GlobalSetting.get_instance
+  end
+
+  #共通設定を更新
+  def update
+    @global_setting = GlobalSetting.get_instance
+    if @global_setting.update_attributes(params[:global_setting])
+      redirect_to "/", :notice => "共通設定を更新しました。"
+    else
+      render :action => "edit"
+    end
+  end
+end
