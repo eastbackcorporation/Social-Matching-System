6e34e6abb4966ce981d3f77b05441254272fb0db
kou honda <kou_honda@eastback.jp>
2012-05-11 10:07:05 +0900

    Update Massage Controller [add ver2.0]

--- app/controllers/receiver/massages_controller.rb
+++ app/controllers/receiver/massages_controller.rb
@@ -5,7 +5,7 @@ class Receiver::MassagesController < MassagesController
   before_filter :require_user
   before_filter :check_receiver
   before_filter :check_validated_datetime
-  
+
   respond_to :html,:json
 
   #自分宛の依頼情報一覧
@@ -50,25 +50,6 @@ class Receiver::MassagesController < MassagesController
   def reject
     @matching_user=MatchingUser.where(:massage_id=>params[:id],:user_id=>current_user.id).first
     @matching_user.update_attributes(:reject_flg=>true)
-
-    @matching_users=MatchingUser.where(:massage_id=>params[:id])
-
-    if self.check_all_reject
-      @massage=Massage.find(params[:id])
-      @massage.update_attributes(:status_id=>3)#status_id 3: 該当者不受理
-    end
     redirect_to(receiver_massages_path, :notice => "お断りしました")
-  end
-
-  protected
-  #massageに紐付く全てのreceiverがrejectしているかチェックする
-  def check_all_reject
-    all_reject_flg=true
-    @matching_users.each do |m|
-      unless m.reject_flg
-        all_reject_flg=false
-      end
-    end
-    return all_reject_flg
   end
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,6 @@
 class Status < ActiveRecord::Base
   attr_accessible :name,:active_flg
   has_many :massages
+
+  scope :to,lambda { |name| where(:name=>name) }
 end
