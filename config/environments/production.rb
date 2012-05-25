b9da0c32a69dc6c444eaf8975c7073ac6777e0ad
kou honda <kou_honda@eastback.jp>
2012-05-25 09:44:52 +0900

    Updated Production enviroments Config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ SocialMatchingSystem::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -49,8 +49,15 @@ SocialMatchingSystem::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port => 25,
+    :domain => 'localhost.localdomain',
+    :authentication => 'plain',
+    :enable_starttls_auto => false
+  }
   # Enable threaded mode
   # config.threadsafe!
 
